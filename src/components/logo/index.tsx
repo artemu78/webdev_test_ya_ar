9becf06722de29fc56fb98c8c6071b928ce4b5ce
Artem Reva <artem.reva@inspirebrands.com>
2022-10-27 17:59:13 +0300

    Remove useLayoutEffect playground

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -5,25 +5,27 @@ const Logo = () => {
     const [color, setColor] = useState<string>("red");
     const logoRef = useRef<HTMLDivElement>(null);
     
-    const sleep = (duration: number) => {
-      let start = new Date().getTime();
-      let end = start;
-      while (end < start + duration) {
-        end = new Date().getTime();
-      }
-    };
+    //uncomment code to see useLayoutEffect usage
+
+    // const sleep = (duration: number) => {
+    //   let start = new Date().getTime();
+    //   let end = start;
+    //   while (end < start + duration) {
+    //     end = new Date().getTime();
+    //   }
+    // };
   
-    useLayoutEffect(() => {
-      console.log(`useLayoutEffect BEFORE sleep(3sec) getElementById color=${document?.getElementById("logowrapper")?.style.fill}`);
-      console.log(`useLayoutEffect BEFORE sleep(3sec) logoRef color=${logoRef.current?.style.fill}`);
-      sleep(3000);
-      console.log(`useLayoutEffect AFTER sleep(3sec) getElementById color=${document?.getElementById("logowrapper")?.style.fill}`);
-      console.log(`useLayoutEffect AFTER sleep(3sec) logoRef color=${logoRef.current?.style.fill}`);
-    }, [color]);
+    // useLayoutEffect(() => {
+    //   console.log(`useLayoutEffect BEFORE sleep(3sec) getElementById color=${document?.getElementById("logowrapper")?.style.fill}`);
+    //   console.log(`useLayoutEffect BEFORE sleep(3sec) logoRef color=${logoRef.current?.style.fill}`);
+    //   sleep(3000);
+    //   console.log(`useLayoutEffect AFTER sleep(3sec) getElementById color=${document?.getElementById("logowrapper")?.style.fill}`);
+    //   console.log(`useLayoutEffect AFTER sleep(3sec) logoRef color=${logoRef.current?.style.fill}`);
+    // }, [color]);
   
-    useEffect(() => {
-      console.log(`useEffect color=${document?.getElementById("logowrapper")?.style.fill}`);
-    }, [color]);
+    // useEffect(() => {
+    //   console.log(`useEffect color=${document?.getElementById("logowrapper")?.style.fill}`);
+    // }, [color]);
   
     const changeColor = () => {
       setColor((color): string => (color === "red" ? "green" : "red"));
